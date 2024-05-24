ba157cd920920b00ecf5989d2ab94431f7812cee
toukali <141361989+toukali@users.noreply.github.com>
2024-05-24 16:08:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,54 @@
-#include <SDL2/SDL.h>
+//Using SDL and standard IO
+#include <SDL.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 int main()
 {
+	//The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+	//Initialize SDL
 	if(SDL_Init(SDL_INIT_VIDEO) != 0){
-	//	std::cout << "Error:" << SDL_GetError();
-	}
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );	}
 	else
 	{
-		SDL_Window* window = SDL_CreateWindow("maze",0,0,800,600,SDL_WINDOW_SHOWN);
+		//Create window
+		window = SDL_CreateWindow("maze",0,0,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
+		if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+		else
+		{
+			 //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+		}
+
 		SDL_Delay(5000);
-		SDL_DestroyWindow(window);
+
+		
 	}
 
+	//Destroy window	
+	SDL_DestroyWindow( window );
+	
+	//Quit SDL subsystems
 	SDL_Quit();
 
 	return 0;
