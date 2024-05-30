dbb9547d8af716e4b64d4deb3237b41ea20d2d92
toukali <toukalisamir@outlook.com>
2024-05-30 23:49:04 +0100

    maze game v0.1

--- main.c
+++ main.c
@@ -1,55 +1,187 @@
-//Using SDL and standard IO
-#include <SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+#include "config.h"
+#include "raycaster.h"
+#include "renderer.h"
+#include "player.h"
+#include "map.h"
 
-int main()
-{
-	//The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] = {
+    {R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R},
+    {R,B,0,0,0,0,P,0,B,R,R,R,R,R,R,R,R,R,R,0,0,0,0,0,0,0,0,0,0,0,R,R},
+    {R,0,0,0,0,0,0,0,0,R,R,R,R,R,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R},
+    {R,0,G,G,0,0,G,0,0,R,R,R,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R},
+    {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R},
+    {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R},
+    {R,0,0,G,0,0,G,0,0,G,R,R,R,R,R,R,R,R,R,0,0,0,0,0,0,0,0,0,0,0,0,R},
+    {R,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,0,0,0,0,R,R,R},
+    {R,B,0,0,B,0,0,0,B,R,R,R,R,R,R,R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,R},
+    {R,R,R,R,R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,0,R,R},
+    {R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,0,R,R},
+    {R,B,0,0,0,0,P,0,B,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,0,R,R},
+    {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,0,R,R},
+    {R,0,G,G,0,0,G,0,0,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,0,R,R},
+    {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,0,R,R},
+    {R,0,0,0,0,0,0,0,0,R,R,R,0,R,0,0,R,R,0,0,B,B,B,B,B,B,B,B,B,0,R,R},
+    {R,0,0,G,0,0,G,0,0,R,R,R,0,R,0,0,R,R,0,0,0,0,0,0,R,0,0,0,0,0,R,R},
+    {R,0,0,0,0,0,0,0,0,R,0,0,0,R,0,0,R,R,0,0,0,0,0,0,R,0,0,0,0,R,R,R},
+    {R,B,0,0,B,0,0,0,B,R,0,R,R,R,0,0,R,R,0,0,0,0,0,0,R,0,0,0,0,R,R,R},
+    {R,R,R,R,R,0,R,0,R,R,0,0,R,R,0,0,R,R,0,0,0,0,0,0,R,R,R,R,0,R,R,R},
+    {R,R,R,R,R,0,0,0,R,R,0,R,R,R,0,0,R,R,R,R,0,R,R,R,0,0,0,R,0,R,R,R},
+    {R,B,0,0,0,0,P,0,B,R,0,R,R,R,0,0,R,R,R,R,0,R,R,R,R,R,0,R,0,R,R,R},
+    {R,0,0,0,0,0,0,0,0,R,0,R,R,R,0,0,R,R,R,R,0,0,0,0,0,0,0,0,0,0,R,R},
+    {R,0,G,G,0,0,G,0,0,R,0,R,R,R,0,0,R,R,R,R,0,R,R,R,R,R,R,R,R,0,R,R},
+    {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,0,0,0,R,R,R,R,R,R,0,R,R},
+    {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,0,R,R,R,0,R,0,0,0,0,R,R},
+    {R,0,0,G,0,0,G,0,0,R,R,G,G,G,G,R,R,R,R,R,0,R,0,0,0,R,0,0,0,R,R,R},
+    {R,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R,R,0,R,R,R,0,R,0,R,R,R,R,R},
+    {R,B,0,0,B,0,0,0,B,R,R,R,R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R},
+    {R,R,R,R,R,R,R,R,R,R,R,R,R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R},
+    {R,0,0,0,0,0,0,0,0,R,R,R,R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R},
+    {R,B,0,0,B,0,0,0,B,R,R,R,R,R,R,R,R,R,R,R,0,R,R,R,R,R,R,R,R,R,R,R}
+};
 
-	//Initialize SDL
-	if(SDL_Init(SDL_INIT_VIDEO) != 0){
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("maze",0,0,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
-		if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+
+/* Program globals */
+Uint32* screenBuffer    = NULL;
+Uint32* redXorTexture   = NULL;
+Uint32* greenXorTexture = NULL;
+Uint32* blueXorTexture  = NULL;
+Uint32* grayXorTexture  = NULL;
+
+const Uint32 COLORS[4] = {
+    RGBtoABGR(255, 0, 0),
+    RGBtoABGR(0, 255, 0),
+    RGBtoABGR(0, 0, 255),
+    RGBtoABGR(128, 128, 128)
+};
+
+Uint32* TEXTURES[4];
+
+/* Program toggles */
+char gameIsRunning    = TRUE;
+char showMap          = TRUE;
+char rayCastMode      = 0;
+
+void render() {
+    if(showMap) {
+        clearRenderer();
+        renderOverheadMap();
+    } else { /* Draw projected scene */
+        renderProjectedScene();
+    }
+}
+
+void consumeSDLEvents() {
+    SDL_Event event;
+    char keyIsDown;
+
+    while(SDL_PollEvent(&event)) {
+        keyIsDown = (event.type == SDL_KEYDOWN);
+        switch(event.type) {
+            case SDL_KEYUP:
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym) {
+                    case SDLK_UP:
+                        movingForward = keyIsDown;
+                        break;
+                    case SDLK_DOWN:
+                        movingBack = keyIsDown;
+                        break;
+                    case SDLK_LEFT:
+                        turningLeft = keyIsDown;
+                        break;
+                    case SDLK_RIGHT:
+                        turningRight = keyIsDown;
+                        break;
+                    case SDLK_LSHIFT:
+                    case SDLK_RSHIFT:
+                        playerIsRunning = keyIsDown;
+                        break;
+                    case SDLK_ESCAPE:
+                        gameIsRunning = FALSE;
+                        break;
+                    
+                    case SDLK_m:
+                        if(keyIsDown) showMap = !showMap;
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+            case SDL_QUIT:
+                gameIsRunning = FALSE;
+                break;
+            default:
+                break;
         }
-		else
-		{
-			 //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+    }
+}
 
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
+void runGame() {
+    long gameTicks = 0;
+    long time;
 
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
+    do {
+        time = SDL_GetTicks();
 
-		SDL_Delay(5000);
+        /* Handle SDL key events */
+        consumeSDLEvents();
 
-		
-	}
+        /* Update the player */
+        updatePlayer();
 
-	//Destroy window	
-	SDL_DestroyWindow( window );
-	
-	//Quit SDL subsystems
-	SDL_Quit();
+        /* Update the raycaster */
+        updateRaycaster();
 
-	return 0;
+        /* Render a frame */
+        render();
+
+        /* Fixed delay before next frame */
+        SDL_Delay(10);
+
+        /* Print FPS every 500 frames */
+        if(!(gameTicks++ % 500))
+            fprintf(stderr, "FPS: %.2f\n", 1000.0f / (float)(SDL_GetTicks() - time));
+    } while(gameIsRunning);
+}
+
+int setupWindow() {
+    int x, y;
+
+    if(!initGFX("Raycaster", WINDOW_WIDTH, WINDOW_HEIGHT)) return FALSE;
+
+    screenBuffer = createTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
+    redXorTexture = generateRedXorTexture(TEXTURE_SIZE);
+    greenXorTexture = generateGreenXorTexture(TEXTURE_SIZE);
+    blueXorTexture = generateBlueXorTexture(TEXTURE_SIZE);
+    grayXorTexture = generateGrayXorTexture(TEXTURE_SIZE);
+    TEXTURES[0] = redXorTexture;
+    TEXTURES[1] = greenXorTexture;
+    TEXTURES[2] = blueXorTexture;
+    TEXTURES[3] = grayXorTexture;
+
+    if(!screenBuffer) return FALSE;
+
+    /* Make the texture initially gray */
+    for(x = 0; x < WINDOW_WIDTH; x++)
+        for(y = 0; y < WINDOW_HEIGHT; y++)
+            screenBuffer[(WINDOW_WIDTH * y) + x] = 0xFFAAAAAA;
+
+    return TRUE;
+}
+
+int main() {
+    if(!setupWindow()) {
+        fprintf(stderr, "Could not initialize raycaster!\n");
+        return EXIT_FAILURE;
+    }
+    initPlayer();
+    initRaycaster();
+    runGame();
+
+    destroyGFX();
+    return EXIT_SUCCESS;
 }
